9596b1aaa488cbfc14d1768e0ad3ff74258b90b8
Вячеслав Чепелев <disillusions@yandex.ru>
2018-01-25 01:54:09 +0300

    debugging utils #1

--- steampy/utils.py
+++ steampy/utils.py
@@ -10,9 +10,15 @@ from steampy.models import GameOptions
 
 
 def text_between(text: str, begin: str, end: str) -> str:
-    start = text.index(begin) + len(begin)
-    end = text.index(end, start)
-    return text[start:end]
+    try:
+        start = text.index(begin) + len(begin)
+        end = text.index(end, start)
+        return text[start:end]
+    except Exception:
+        print('>>> ERROR in text_between.')
+        print('TEXT:')
+        print(text)
+        return ''
 
 
 def texts_between(text: str, begin: str, end: str):
